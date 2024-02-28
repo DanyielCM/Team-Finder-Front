502329ca43ea6a8f2c60c2908c106ecb48c7021a
david <david.boicu@student.usv.ro>
2024-02-28 15:29:16 +0200

    Main-page and Org. Register Page

---
+++ frontend/src/pages/start-page.js
@@ -0,0 +1,32 @@
+import "./start-page.css";
+import { Outlet, Link } from "react-router-dom";
+
+import Navbar from "../components/start-page-navbar.js";
+
+export default function StartPage() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="main-page">
+        <div className="left-container">
+          <div className="main-header">
+            <h1>TEAMFINDER</h1>
+            <h2>Uniting Skills, Building Teams</h2>
+            <h4>
+              Discover your perfect project team effortlessly with TeamFinder!
+            </h4>
+          </div>
+
+          <div className="create-org">
+            <h2>Launch Your Organization's Journey</h2>
+            <button type="button" className="btn">
+              <Link to="/register">Create an organisation</Link>{" "}
+            </button>
+          </div>
+        </div>
+
+        <div className="right-container"></div>
+      </div>
+    </>
+  );
+}
