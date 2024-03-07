f49f92aa708c6205fbda4e1e230ae3026929180d
david <david.boicu@student.usv.ro>
2024-03-07 11:05:19 +0200

    Added form data validation

--- frontend/README.md
+++ frontend/README.md
@@ -1,11 +1,1 @@
-## Important
-### Branch Workflow
-- for each task, create a new branch starting from the main branch and name it feature/<feature_name>
-- after you complete the task and it's a stable version merge the feature branch into the main branch
-
-## Frontend
-### Project installation
-- npm install
-
-### Running local server
-- npm run dev
+### Comments to be added
---
+++ frontend/src/pages/org-register-page.jsx
@@ -0,0 +1,79 @@
+import "./org-register-page.css";
+import Navbar from "../components/start-page-navbar.jsx";
+
+import Form from "../components/common/form.jsx";
+
+import React, { useState } from "react";
+
+import UserRegisterService from "../services/register.service";
+
+import { useNavigate } from "react-router-dom";
+
+export default function OrgRegisterPage() {
+  const navigateTo = useNavigate();
+
+  const fields = [
+    {
+      name: "employeeUserName",
+      label: "Name",
+      type: "text",
+      placeholder: "Name(name of individual)",
+    },
+    {
+      name: "employeeEmail",
+      label: "Email",
+      type: "email",
+      placeholder: "Email",
+    },
+    {
+      name: "employeePassword",
+      label: "Password",
+      type: "password",
+      placeholder: "Password",
+    },
+    {
+      name: "organizationName",
+      label: "Organisation Name",
+      type: "text",
+      placeholder: "Name of organisation",
+    },
+    {
+      name: "hqAddress",
+      label: "Headquarter Address",
+      type: "text",
+      placeholder: "Headquarter address",
+    },
+  ];
+
+  const handleSubmit = (formData) => {
+    console.log(formData);
+    UserRegisterService.registerOrg(formData)
+      .then((isRegistered) => {
+        if (isRegistered) {
+          alert("Success");
+          navigateTo("/login");
+        } else {
+          console.error("Registration failed.");
+          alert("Something went wrong!");
+        }
+      })
+      .catch((error) => {
+        console.error("Error during registration:", error);
+        // Handle the error
+      });
+  };
+
+  return (
+    <div className="background">
+      <Navbar></Navbar>
+      <div className="register-main-page">
+        <Form
+          title="Organisation Register"
+          subtitle="Please fill out the form below for register"
+          onSubmit={handleSubmit}
+          fields={fields}
+        />
+      </div>
+    </div>
+  );
+}
